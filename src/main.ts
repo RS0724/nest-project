2988b4caffac7ac0edc83a3f0b0f904f9bfb8dce
RS0724 <rosesilva0724@gmail.com>
2023-10-09 21:24:59 -0300

    fix: prisma auth to user

--- src/main.ts
+++ src/main.ts
@@ -4,22 +4,26 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const port = 3000 || process.env.PORT
+  const port = 3000 || process.env.PORT;
 
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
 
-  const config = new DocumentBuilder().setTitle('Nest-Project').addServer('http://localhost:3000', 'Local').addTag('Status').addTag('Auth').build()
-  
+  const config = new DocumentBuilder()
+    .setTitle('Nest-Project')
+    .addServer('http://localhost:3000', 'Local')
+    .addTag('Status')
+    .addTag('User')
+    .build();
+
   const document = SwaggerModule.createDocument(app, config);
 
   SwaggerModule.setup('docs', app, document);
 
   await app.listen(port);
-  
-  console.log("http://localhost:" + port)
 
-
+  console.log('http://localhost:' + port + '/docs');
 }
+
 bootstrap();
---
+++ src/user/user.controller.ts
@@ -0,0 +1,45 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { UserService as UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+
+@ApiTags('User')
+@Controller('User')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  @ApiOperation({summary: 'Criar novo usúario'})
+  create(@Body() createAuthDto: CreateUserDto) {
+    try {
+      return this.userService.create(createAuthDto);      
+    } catch (error) {
+     console.log(error) 
+    }
+  }
+
+  @Get()
+  @ApiOperation({summary: 'Pesquisa todos os usuários'})
+  findAll() {
+    return this.userService.findAll();
+  }
+
+  @Get(':id')
+  @ApiOperation({summary: 'Pesquisa um usuário por id'})
+  findOne(@Param('id') id: string) {
+    return this.userService.findOne(+id);
+  }
+
+  @Patch(':id')
+  @ApiOperation({summary: 'Alterar dados do usuário'})
+  update(@Param('id') id: string, @Body() updateAuthDto: UpdateUserDto) {
+    return this.userService.update(+id, updateAuthDto);
+  }
+
+  @Delete(':id')
+  @ApiOperation({summary: 'Deletar um usuário'})
+  remove(@Param('id') id: string) {
+    return this.userService.remove(+id);
+  }
+}
